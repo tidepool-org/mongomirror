e8c9f4d9690f551fca172f999e5577e4572699dc
Derrick Burns <derrick@tidepool.org>
2020-01-28 18:18:31 -0800

    Update travis script

--- artifact.sh
+++ artifact.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 
-wget -q -O artifact_go.sh 'https://raw.githubusercontent.com/tidepool-org/tools/master/artifact/artifact.sh'
-chmod +x artifact_go.sh
+wget -q -O artifact.sh 'https://raw.githubusercontent.com/tidepool-org/tools/master/artifact/artifact.sh'
+chmod +x artifact.sh
 
-. ./version.sh
-./artifact_go.sh go
+./artifact.sh
